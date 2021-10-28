441336b5c75125860b2cfb96a2ca2ff29c519f70
Srusti Thakkar <srusti.thakkar@cygnet.com>
2021-10-28 16:04:10 +0530

    filter code

--- script.js
+++ script.js
@@ -2,18 +2,6 @@ const container= document.querySelector(".container")
 const seat = document.querySelector(".seat")
 let totalSeat = [];
 
-// function moviePrice (){
-//   let movies = document.getElementById("movie")
-//   const price = movies.value
-//   document.getElementById("price").innerHTML = price;
-//   document.getElementById("total").innerHTML = price;
-// }
-
-  // container.addEventListener('click', (e) => {
-  //   document.getElementById(e).seat.selected
-  // }
-  // ) 
-
 function seatStatus (e) {
   if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
     e.target.classList.toggle('selected');
