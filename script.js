c847c8133c42b9700866a8998e6a64ac555b29e3
Srusti Thakkar <srusti.thakkar@cygnet.com>
2021-10-29 11:28:30 +0530

    bug fixes

--- script.js
+++ script.js
@@ -2,10 +2,24 @@ const container= document.querySelector(".container")
 const seat = document.querySelector(".seat")
 let totalSeat = [];
 
+function totalSeatValue(id){
+  if(totalSeat.includes(id)){
+    totalSeat.pop(id);
+  }
+  else {
+    totalSeat.push(id)
+  }
+}
+
+function onMovieChange(value){
+  console.log(value)
+  seatStatus()
+}
+
 function seatStatus (e) {
   if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
     e.target.classList.toggle('selected');
-    totalSeat.push(e.target.id)
+    totalSeatValue(e.target.id)
     let seats = totalSeat.length;
     document.getElementById("count").innerHTML = seats;
     let movies = document.getElementById("movie")
